185e1c51c5156ac2ad97563fa4508b68fcb5562c
Ekis <ekissuper@gmail.com>
2013-02-09 15:42:24 +0100

    Some small stuff

--- Game/Assets/Scripts/GenericScripts/MapScripts/MapGenerationScript.cs
+++ Game/Assets/Scripts/GenericScripts/MapScripts/MapGenerationScript.cs
@@ -4,14 +4,16 @@ using System.Collections;
 public class MapGenerationScript : MonoBehaviour {
 	
 	
-	public Transform wall;
+	public GameObject wall;
 
 	void Start() {
-		Instantiate(wall, new Vector3(0, 1, 0), Quaternion.identity);
+		GameObject obj = Instantiate(wall, new Vector3(0, 1, 0), Quaternion.identity);
+		//GameObject test = (GameObject) obj as GameObject;
+		obj.transform.Rotate(new Vector3(0,90,0));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
