062e9eea7430eec5dbae5a21d2979a62fd2b68fe
Ekis <ekissuper@gmail.com>
2013-02-22 00:35:25 +0100

    Fixed skeletons for emote and sound-commands

--- Game/Assets/Scripts/GenericScripts/Commands/SoundCommand.cs
+++ Game/Assets/Scripts/GenericScripts/Commands/SoundCommand.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 using System.Collections;
 
 public class SoundCommand : ICommand {
-
+	
+	public SoundCommand(Vector3 location, string sound) {
+		
+	}
+	
 	public bool isAllowed() {
 		return true;
 	}
 	
 	public void execute() {
-		
+		//playsound
 	}
 }
