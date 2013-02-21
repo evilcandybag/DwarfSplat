fd140a62227f85792ad34d10e865aa01915f1876
Peter <peter@insella.se>
2013-02-21 22:55:13 +0100

    Support for creating Decorators and then adding a Child, to support different kinds of workflows.

--- Game/Assets/Scripts/GenericScripts/BehaviorTrees/ConditionDecorator.cs
+++ Game/Assets/Scripts/GenericScripts/BehaviorTrees/ConditionDecorator.cs
@@ -6,6 +6,24 @@ namespace BehaviorTrees
 	{
 		private Func<bool> condition;
 		
+		/// <summary>
+		/// Construct a childless ConditionDecorator with the given condition.
+		/// </summary>
+		/// <param name='condition'>
+		/// Condition on which to execute Child.
+		/// </param>
+		public ConditionDecorator(Func<bool> condition) : base(){
+			this.condition = condition;
+		}
+		/// <summary>
+		/// Construct a ConditionDecorator with the given Child and condition.
+		/// </summary>
+		/// <param name='child'>
+		/// Child.
+		/// </param>
+		/// <param name='condition'>
+		/// Condition on which to execute Child.
+		/// </param>
 		public ConditionDecorator (Node child, Func<bool> condition) : base(child) 
 		{
 			this.condition = condition;
