dcfc9166dec06847689c02c7774c270f22cf2d79
Ekis <ekissuper@gmail.com>
2013-02-21 18:28:50 +0100

    nothing particular, temp solution

--- Game/Assets/Scripts/GameScripts/GameStuff/Actors/AbstractAIActor.cs
+++ Game/Assets/Scripts/GameScripts/GameStuff/Actors/AbstractAIActor.cs
@@ -5,5 +5,35 @@ using UnityEngine;
 public abstract class AbstractAIActor : MonoBehaviour, IAgent, IActor
 {
 	public abstract void RunAI();
+	IItem inventory;
+	
+	public IItem[] getInventory() {
+		
+		return new IItem[]{inventory};
+	}
+	
+	public bool useItem(IItem item) {
+		if (hasItem(item)) {
+			item.use(this);
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public bool addItem(IItem item) {
+		inventory = item;
+		return true;
+		
+	}
+	
+	public bool hasItem(IItem item) {
+		if (item.Equals(inventory)) {
+			return true;
+		}
+		else {	
+			return false;
+		}
+	}
 }
 
