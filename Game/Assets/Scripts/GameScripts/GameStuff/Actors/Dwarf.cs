6f28ec8d85cdd534cf83d9c688711d8f996dd577
Peter <peter@insella.se>
2013-02-23 14:18:38 +0100

    Work on dwarf-specific behaviors.

--- Game/Assets/Scripts/GameScripts/GameStuff/Actors/Dwarf.cs
+++ Game/Assets/Scripts/GameScripts/GameStuff/Actors/Dwarf.cs
@@ -1,45 +1,50 @@
 using UnityEngine;
 using System.Collections;
+using BehaviorTrees;
 
-public class Dwarf : MonoBehaviour, IActor {
+public class Dwarf : AbstractAIActor {
 	
-	IItem inventory;
+	private IInteractable bed_, work_;
+	private DwarfBehavior behavior;
+	public Status state;
 	
+	public static readonly double SLEEP_RATE = 1, WORK_RATE = 0.5, IDLE_RATE = 0.1, FLEE_RATE = 0.8;
+	
+	public enum Status {
+		SLEEP,
+		WORK,
+		FLEE,
+		IDLE //including moving between the other tasks
+	}
+		
 	// Use this for initialization
 	void Start () {
+		behavior = new DwarfBehavior(this);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-	
-	public IItem[] getInventory() {
-		
-		return new IItem[]{inventory};
-	}
-	
-	public bool useItem(IItem item) {
-		if (hasItem(item)) {
-			item.use(this);
-			return true;
-		}
-		else
-			return false;
-	}
-	
-	public bool addItem(IItem item) {
-		inventory = item;
-		return true;
-		
-	}
-	
-	public bool hasItem(IItem item) {
-		if (item.Equals(inventory)) {
-			return true;
-		}
-		else {	
-			return false;
+		switch (state) {
+		case Status.SLEEP:
+			behavior.Sleep.Prio -= Time.deltaTime * SLEEP_RATE;
+			break;
+		case Status.WORK:
+			behavior.Sleep.Prio += Time.deltaTime * WORK_RATE;
+			break;
+		case Status.FLEE:
+			behavior.Sleep.Prio += Time.deltaTime * FLEE_RATE;
+			break;
+		default:
+			behavior.Sleep.Prio += Time.deltaTime * IDLE_RATE;
+			break;
 		}
 	}
+	
+	public override void RunAI(){
+		behavior.Run();
+	}
+	//TODO: dunno if this is the right way to go about it or if we should use commands for that shit
+	public IInteractable Bed { get { return bed_;} }
+	public IInteractable Workplace { get { return work_;} }
+	
 }
