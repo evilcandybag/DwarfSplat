0bab527221de6e06b2cabff087fea9d04f78467e
Peter <peter@insella.se>
2013-02-23 15:04:43 +0100

    started work on dwarf emotes AI

---
+++ Game/Assets/Scripts/GameScripts/AI/Behavior/DwarfEmotes.cs
@@ -0,0 +1,15 @@
+using System;
+
+/// <summary>
+/// Behavior tree wrapper for handling emotes and sounds for dwarves. 
+/// </summary>
+public class DwarfEmotes
+{
+	
+	
+	
+	public DwarfEmotes ()
+	{
+	}
+}
+
