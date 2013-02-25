bf3fde95a15a03b6b4c0491d43895003884c7bf1
Peter <peter@insella.se>
2013-02-25 14:40:58 +0100

    Some more work on emotes.

--- Game/Assets/Scripts/GameScripts/AI/Behavior/DwarfEmotes.cs
+++ Game/Assets/Scripts/GameScripts/AI/Behavior/DwarfEmotes.cs
@@ -1,15 +1,21 @@
 using System;
-
+using BehaviorTrees;
+using BehaviorTrees.Unity;
 /// <summary>
 /// Behavior tree wrapper for handling emotes and sounds for dwarves. 
 /// </summary>
 public class DwarfEmotes
 {
+	private Node root;
+	private static readonly double COOLDOWN = 10.0;
 	
-	
-	
-	public DwarfEmotes ()
+	public DwarfEmotes (Dwarf d)
 	{
+		root = new UnityCooldownDecorator(COOLDOWN);
+	}
+	
+	public void Run() {
+		root.Visit();
 	}
 }
 
