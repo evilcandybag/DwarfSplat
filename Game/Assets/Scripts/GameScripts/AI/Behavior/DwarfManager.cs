7449f4d896f993566d4b2a006f75bd14c687734b
Peter <peter@insella.se>
2013-02-27 22:05:04 +0100

    stuff´

--- Game/Assets/Scripts/GameScripts/AI/Behavior/DwarfManager.cs
+++ Game/Assets/Scripts/GameScripts/AI/Behavior/DwarfManager.cs
@@ -5,7 +5,7 @@ public class DwarfManager : BehaviorManager<int,Dwarf>
 {
 	private int counter;
 	
-	protected override void Start() {
+	public override void Start() {
 		base.Start();
 		counter = 0;
 	}
