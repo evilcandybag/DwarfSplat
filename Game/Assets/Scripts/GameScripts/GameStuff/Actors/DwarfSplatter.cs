714f167091cab12e9c4a82ef5c1b2261f5ca77a6
Peter <peter@insella.se>
2013-03-07 14:34:09 +0100

    Added sound effects to explosions and splatting!

--- Game/Assets/Scripts/GameScripts/GameStuff/Actors/DwarfSplatter.cs
+++ Game/Assets/Scripts/GameScripts/GameStuff/Actors/DwarfSplatter.cs
@@ -3,16 +3,28 @@ using System.Collections;
 
 public class DwarfSplatter : MonoBehaviour {
 	private int score;
+	private AudioSource aus;
+	
+	public AudioClip splat1;
+	public AudioClip splat2;
 	
 	void Awake() {
 		score = 0;
 	}
 	// Use this for initialization
 	void OnCollisionEnter(Collision collision) {
+		if (aus == null) {
+			aus = gameObject.GetComponent<AudioSource>();
+		}
 		// check if this is a dwarf. 
 		GameObject col = collision.contacts[0].otherCollider.gameObject;
 		Dwarf d = col.GetComponent<Dwarf>();
 		if (d != null) {
+			if (Random.value > 0.5) {
+				aus.PlayOneShot(splat1);
+			} else {
+				aus.PlayOneShot(splat2);
+			}
 			ActorController.getActorController().destoyActor(d);
 			score++;
 		}
--- Game/Assets/Scripts/GameScripts/Graphics/ExplotionScript.cs
+++ Game/Assets/Scripts/GameScripts/Graphics/ExplotionScript.cs
@@ -8,8 +8,21 @@ using System.Collections;
 public class ExplotionScript : MonoBehaviour {
 
 	public GameObject explotion;
+	public AudioClip expl1;
+	public AudioClip expl2;
+	
+	private AudioSource aus;
 	
 	public void Explode(Vector3 position){
-		 Instantiate(explotion, position, Quaternion.identity);
+		if (aus == null) {
+			//horrible =(
+			aus = GameObject.Find("Ball(Clone)").GetComponent<AudioSource>();
+		}
+		if (Random.value > 0.5) {
+			aus.PlayOneShot(expl1,1);
+		} else {
+			aus.PlayOneShot(expl2,1);
+		} 
+		Instantiate(explotion, position, Quaternion.identity);
 	}
 }
